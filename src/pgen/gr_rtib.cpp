fed590fed12703e21b410dd0abd82c5e69e2cf1c
Sean Ressler <smressle@berkeley.edu>
2025-01-31 13:10:06 -0500

    iave

--- src/pgen/gr_rtib.cpp
+++ src/pgen/gr_rtib.cpp
@@ -581,7 +581,7 @@ void MeshBlock::ProblemGenerator(ParameterInput *pin) {
 
             pfield->b.x3f(k,j,i) = b3 * u0 - b0 * u3;
 
-            if std::isnan(pfield->b.x3f(k,j,i)){
+            if (std::isnan(pfield->b.x3f(k,j,i))){
               fprintf(stderr,"NAN in B3!!\n b3: %g u0: %g b0: %g u3: %g\n g: %g %g %g %g \n",b3,u0,b0,u3, g(I00),g(I11),g(I22),g(I33));
             }
           }
