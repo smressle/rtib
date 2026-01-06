b0ae648b658d681a4e423e23cc11a412bd43061d
Sean Ressler <smressle@berkeley.edu>
2026-01-06 11:42:35 -0500

    ivae

--- src/pgen/gr_rtib_smooth.cpp
+++ src/pgen/gr_rtib_smooth.cpp
@@ -452,7 +452,8 @@ void Mesh::InitUserMeshData(ParameterInput *pin) {
     // sigma_h/sigma_c = Bh^2/Bc^2 * drat
     // Bh^2/Bc^2 = 1 + (1 - 1/drat)*beta_c 
     // sigma_h/sigma_c  = drat + (drat -1)*beta_c
-    drat = ( sigma_h/sigma_c+beta_c )/(1.0 + beta_c);
+    // drat = ( sigma_h/sigma_c+beta_c )/(1.0 + beta_c);
+    drat = ( sigam_h/sigma_c ) * ( 1.0 + beta_h ) / ( 1.0 + beta_c) ;
 
     rho_c = rho_h * drat;
 
